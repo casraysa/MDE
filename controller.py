cf4ec5b835db8b7641fb3a767e234295fbe1fe40
Salvador Castillo Raya <casraysa@hotmail.com>
2023-01-29 19:53:28 +0100

    Resto de ficheros necesarios para el proyecto

--- controller.py
+++ controller.py
@@ -2,13 +2,10 @@ from . import models
 from imagekitio import ImageKit
 import json
 import requests as r
-
+from uuid import uuid4
 
 with open("credentials.json") as f:
     creds = json.load(f)
-
-api_key = creds['imaggacreds']['api_key']
-api_secret = creds['imaggacreds']['api_secret']
 
 imagekit = ImageKit(
     private_key = creds['imagekitcreds']['private_key'],
@@ -16,16 +13,16 @@ imagekit = ImageKit(
     url_endpoint ='https://ik.imagekit.io/casraysa'
 )
 
+api_key = creds['imaggacreds']['api_key']
+api_secret = creds['imaggacreds']['api_secret']
 
-def upload_photo(imgstr, photo_path):
-    photo_name = photo_path.replace('./images/', '')
-    upload_info = imagekit.upload(file = imgstr, file_name = photo_name)
-
-    return upload_info
-
-
-def find_tags(image_url, min_confidence):
-    link = f"https://api.imagga.com/v2/tags?image_url={image_url}"
+def get_tags(b64str, min_confidence):
+    
+    photo_name = str(uuid4())
+    # Generamos la URL para luego llamar a la API de imagekitio
+    upload_info = imagekit.upload(file = b64str, file_name = photo_name)
+    
+    link = f"https://api.imagga.com/v2/tags?image_url={upload_info.url}"
     response = r.get(link, auth = (api_key, api_secret))
 
     tags = [
@@ -34,8 +31,12 @@ def find_tags(image_url, min_confidence):
         if t["confidence"] > min_confidence
     ]
     
-    return tags
-
-
-def delete_photo(upload_info):
     imagekit.delete_file(file_id = upload_info.file_id)
+    
+    # Guardamos la imagen en una carpeta determinada
+    file_name = "images/" + photo_name
+    with open(file_name, 'w') as f:
+        f.write(b64str)
+    
+    # Guardamos en BBDD y devolvemos respuesta
+    return models.tags(file_name, tags)
--- views.py
+++ views.py
@@ -5,39 +5,35 @@ bp = Blueprint('images', __name__, url_prefix='/')
 
 @bp.post("/image")
 def process_pict():
+    min_confidence = request.args.get("min_confidence")
+    
+    min_confidence = 80 if min_confidence is None else int(min_confidence)
     
     if not request.is_json or "data" not in request.json:
         return make_response({"description": "Debes incluir la foto en base64 en el campo data del body."}, 400)
     
-    photo_path = request.json["path"]
-    size = request.json["size"]
-    imgstrb64 = request.json["data"]
+    imgb64str = request.json["data"]
         
-    controller.models.add_photo(photo_path, size, imgstrb64)
-
-    photo_info = controller.upload_photo(imgstrb64)
-    tags = controller.find_tags(photo_info.url, 80)
-    controller.delete_photo(photo_info)
-    
-    photo_dict = controller.models.get_photo_dict(photo_path)  
-    controller.models.insert_tags(photo_dict, tags)
-    
-    d = controller.models.get_image(photo_dict['id'])  
+    d = controller.get_tags(imgb64str, min_confidence)
 
     return jsonify(d)
 
 @bp.get("/images")
 def images():
-    d = controller.models.get_images()
+    min_date = request.args.get("min_date")
+    max_date = request.args.get("max_date")
+    
+    d = controller.models.get_images(min_date, max_date)
    
     return jsonify(d)
  
-@bp.get("/image")
-def image():
-    id = request.args.get("id")
-    
+@bp.get("/image/<id>")
+def image(id):
+        
     if id is None:
         return make_response({"description": "Debes especificar un id de photo en la llamada."}, 400)
+    
+    id = int(request.args.get("id"))
     
     d = controller.models.get_image(id)
     
@@ -47,6 +43,8 @@ def image():
 def tags():
     min_date = request.args.get("min_date")
     max_date = request.args.get("max_date")
-    d = controller.models.get_tags(min_date, max_date)
+    tags = request.args.get("tags")
+    
+    d = controller.models.get_tags(min_date, max_date, tags)
     
     return jsonify(d)
